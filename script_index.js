a9ce230fa0cd0e7792c380676b36c1552228ed47
a23pablomt <147613101+a23pablomt@users.noreply.github.com>
2024-11-08 12:43:42 +0100

    Add files via upload

--- script_index.js
+++ script_index.js
@@ -1,28 +1,8 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+// Get the menu button element
+const menuButton = document.getElementsByClassName('button-52');
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+// Add click event listener to the menu button
+menuButton.addEventListener('click', () => {
+    // Open platos.html in a new window or tab
+    window.open('platos.html', '_blank');
+});
